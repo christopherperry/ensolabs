070b27942f4f8bba9605a1bde09a5348bd7fb3da
ZenMasterChris <mr.christopher.perry@gmail.com>
2013-04-11 22:25:58 -0700

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Ensolabs::Application.config.secret_key_base = '2eeb713b286af5227e5c9525c4a877ed05767c14afe96239ad9feb4f293e02701938afdd472c2161b39389fc1cf7d2ced11ec50421d62cbbe09b4303e50929b1'
